6c355b9fdf43efa73c29d93e4675ee78372d57c0
gnfranco92 <guilhermenetofranco@ymail.com>
2020-12-13 11:15:22 -0300

    Criada a classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,5 +3,7 @@ Primeira Classe - criada
 """
 
 # Classe pep 8  - Função letra minuscula e Classe letra maiuscula
+# Coloca o estado de todos arquivos, tirando uma foto, fazendo o Push para enviar no repositorio no GitHub; direito na pasta -> Git - > Commit Directory
+#Subir no GitHub depois da Print, CTRl+SHIFT+K OU direito na pasta -> Git - > PUSH
 class Pessoa:
     pass
