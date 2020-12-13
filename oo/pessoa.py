01d6925c4f5ac1329902643a02ed2bfbb3dc1e1d
gnfranco92 <guilhermenetofranco@ymail.com>
2020-12-13 11:27:19 -0300

    Criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -5,5 +5,15 @@ Primeira Classe - criada
 # Classe pep 8  - Função letra minuscula e Classe letra maiuscula
 # Coloca o estado de todos arquivos, tirando uma foto, fazendo o Push para enviar no repositorio no GitHub; direito na pasta -> Git - > Commit Directory
 #Subir no GitHub depois da Print, CTRl+SHIFT+K OU direito na pasta -> Git - > PUSH
+
+# Atribuir Metode (Função que pertence a uma classe, sempre conectado a um objeto
+
 class Pessoa:
-    pass
+    def cumprimentar(self):    # Metodo: cumprimentar / Objeto:self
+        return f'Olá {id(self)}'  # f string!
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))  # Não é usual executar o método desta maneira
+    print(id(p))
+    print(p.cumprimentar())   # p: objeto, objeto.método, maneira mais usual!!!
