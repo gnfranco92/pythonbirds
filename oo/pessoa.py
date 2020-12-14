b7cda15e0b5015ff25ceebb83985942c781a349f
gnfranco92 <guilhermenetofranco@ymail.com>
2020-12-14 09:56:19 -0300

    Criados atributos de instância nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -10,12 +10,23 @@ Primeira Classe - criada
 # Atribuir Metode (Função que pertence a uma classe, sempre conectado a um objeto
 
 class Pessoa:
+    def __init__(self, nome = None, idade=35):    # Criando método especial -- self.nome ( O nome é o nome do objeto self)
+        self.idade = idade
+        self.nome = nome          # Criando atributo!
+
+    # Atributos de instância e de objetos são criados atreves do metódo __init__
+
     def cumprimentar(self):  # Metodo: cumprimentar / Objeto:self
         return f'Olá {id(self)}'  # f string!
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Guilherme')     # Alterando o nome ja na construção
     print(Pessoa.cumprimentar(p))  # Não é usual executar o método desta maneira
     print(id(p))
-    print(p.cumprimentar())  # p: objeto, objeto.método, maneira mais usual!!!
+    print(p.cumprimentar())  # p: objeto, objeto.método, maneira mais usual!!! - ATRIBUTO DA CLASSE
+    print(p.nome)        # Acessar o atributo atraves do objeto
+    p.nome = 'Guilherme'  # Alterar o valor do atributo!
+    print(p.nome)
+    print(p.idade)
+
