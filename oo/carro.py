64c067cfdda0b33867f2c4608ee6da1170555441
Guilherme Neto Franco <guilhermenetofranco@ymail.com>
2020-12-15 10:58:15 -0300

    Implementado classe Carro

--- oo/carro.py
+++ oo/carro.py
@@ -101,8 +101,8 @@ class Direcao:
     rotacao_a_direita_dct = {NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE}
     rotacao_a_esquerda_dct = {NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL}
 
-    def  __init__(self):
-                self.valor = NORTE         # Norte é a partida da direção!!
+    def __init__(self):
+        self.valor = NORTE         # Norte é a partida da direção!!
 
     def girar_a_direita(self):
         self.valor = self.rotacao_a_direita_dct[self.valor]  # Maneira de não utilizar o If, diminuindo o código.
@@ -127,6 +127,37 @@ class Motor:
 
     def frear(self):
         self.velocidade -= 2
-        self.velocidade=max(0, self.velocidade)    # Função Max - Se existir valor menor que '0', retorna o '0', se não retorna o valor da velocidade.
+        self.velocidade = max(0, self.velocidade)    # Função Max - Se existir valor menor que '0', retorna o '0', se não retorna o valor da velocidade.
+
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.direcao = direcao
+        self.motor = motor
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        return self.motor.acelerar()
+
+    def frear(self):
+        return self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        return self.direcao.girar_a_direita()
+    def girar_a_esquerda(self):
+        return self.direcao.girar_a_esquerda()
+
+
+
+
+
+
+
+
 
 
